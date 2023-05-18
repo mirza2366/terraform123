068d90798946de5a59e364a68bd7cab4f73f6ee3
MIRZA SHAHNAWAZ BAIG <105604124+mirza2366@users.noreply.github.com>
2023-05-18 18:41:50 +0530

    Add files via upload

---
+++ main.tf
@@ -0,0 +1,22 @@
+
+terraform {
+backend "azure" {}
+}
+
+terraform {
+  required_providers {
+    azurerm = {
+      source = "hashicorp/azurerm"
+      version = "3.51.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  # Configuration options
+  features {}
+}
+resource "azurerm_resource_group" "RG" {
+  name     = "RG1"
+  location = "EASTUS"
+}
